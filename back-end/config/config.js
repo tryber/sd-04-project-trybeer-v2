a98e7cb1353934aa497750e7e7e326faeb2805af
igorgbr <igorgphotoarte@gmail.com>
2020-12-16 21:51:20 -0300

    primeiro commit

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -1,28 +1,25 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.SCHEMA,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
-    logging: false,
+  "development": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD, 
+    "database": process.env.SCHEMA, 
+    "host": process.env.HOSTNAME,
+    "dialect": 'mysql',
   },
-  test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.SCHEMA,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
-    logging: false,
+  "test": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.SCHEMA,
+    "host": process.env.HOSTNAME,
+    "dialect": "mysql",
   },
-  production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.SCHEMA,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
-    logging: false,
+  "production": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.SCHEMA,
+    "host": process.env.HOSTNAME,
+    "dialect": 'mysql',
   },
 };
