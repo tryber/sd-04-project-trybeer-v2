6c7901192ba6b883d058e0a15a9af813e28afbb2
JorgeHSOsiro <jojimurakabe@gmail.com>
2021-01-04 23:41:47 -0300

    cc 2

--- back-end/models/productsModel.js
+++ back-end/models/productsModel.js
@@ -1,13 +1,13 @@
 const Products = (sequelize, DataTypes) => sequelize.define(
-    'Product',
-    {
-      name: DataTypes.STRING,
-      price: DataTypes.INTEGER,
-      url_image: DataTypes.STRING,
-    },
-    {
-      timestamps: false,
-    },
-  );
+  'Product',
+  {
+    name: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    url_image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  },
+);
 
 module.exports = Products;
