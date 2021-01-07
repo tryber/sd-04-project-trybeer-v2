d73ca5b464f23a535f5ab997071395a3b49c0065
JorgeHSOsiro <jojimurakabe@gmail.com>
2021-01-06 22:49:44 -0300

    chat client button done

---
+++ front-end/src/pages/ChatClient/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+const ChatClient = () => (
+  <div>
+    <Header title="Finalizar Pedido" dataTestid="top-title" />
+    <div>Chat Client</div>
+  </div>
+);
+
+export default ChatClient;
