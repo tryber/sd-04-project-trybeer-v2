5fcb61a0ca9e4cbffa29537c86e911d397f57829
Mateus-Moreira <ashuraw@hotmail.com>
2021-01-12 18:39:07 -0300

    arrumado problema de redirecionamento por role

--- front-end/src/pages/OrdersDetails/index.js
+++ front-end/src/pages/OrdersDetails/index.js
@@ -1,18 +1,20 @@
 import React, { useEffect } from 'react';
-import { useHistory, useRouteMatch } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 import ClientDetails from './ClientDetails';
 import AdminDetails from './AdminDetails';
 
 const OrderDetails = () => {
   const history = useHistory();
-  const clientRoute = useRouteMatch('/orders/:id');
+  // const clientRoute = useRouteMatch('/orders/:id');
 
   useEffect(() => {
     if (!localStorage.user) history.push('/login');
   }, [history]);
+  const user = jwtDecode(localStorage.user);
   if (localStorage.user) {
-    if (clientRoute) {
+    if (user.dataValues.role === 'client') {
       return (
         <ClientDetails />
       );
