dd3302ea33b24ab64e44c067ba0fa3cf26a75d84
Josias Viveiro <josiasviveiro@gmail.com>
2021-01-12 12:44:31 -0300

    Página Chat criada - estrutura básica

---
+++ front-end/src/pages/Chat/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Flex, Text, Box, Container,
+} from '@chakra-ui/react';
+
+const Chat = () => {
+  return (
+    <Container>
+      <Text>Chat - Conversas com a loja</Text>
+    </Container>
+  );
+};
+
+export default Chat;
