24057228e0a24e28f71dde1af64c795f8e8192e8
Josias Viveiro <josiasviveiro@gmail.com>
2021-01-14 12:02:05 -0300

    Mudança no nome da collection de 'Messages' para 'messages'

--- back-end/modelsMongo/message.js
+++ back-end/modelsMongo/message.js
@@ -3,13 +3,13 @@ const connectionMongo = require('../config/mongoDb');
 // A chave 'chat' é a relação entre loja e cliente
 const addMessage = async (nickname, time, message, timestamp, chat) => {
   const result = await connectionMongo()
-    .then((db) => db.collection('Messages').insertOne({ nickname, time, message, timestamp, chat }));
+    .then((db) => db.collection('messages').insertOne({ nickname, time, message, timestamp, chat }));
   return result.ops[0];
 };
 
 const getMessageByClient = async (chat) => {
   const result = await connectionMongo()
-    .then((db) => db.collection('Messages')
+    .then((db) => db.collection('messages')
       .find({ chat })
       .sort({ timestamp: -1 })
       .toArray());
