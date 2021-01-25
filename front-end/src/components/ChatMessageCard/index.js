b7ead102313181a870ea027ead953774044ebe87
Josias Viveiro <josiasviveiro@gmail.com>
2021-01-25 11:40:53 -0300

    ChatMessageCard - componente para a estrutura das mensagens

---
+++ front-end/src/components/ChatMessageCard/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+
+const ChatMessageCard = ({ msg }) => {
+  const { nickname, time, message } = msg;
+  return (
+    <Box bgGradient="linear(#edf2f7, #dce6ef)" borderRadius="5px">
+      <Text color="#40698c" data-testid="nickname">{ nickname }</Text>
+      <Text color="#40698c" data-testid="message-time">{ time }</Text>
+      <Text data-testid="text-message">{ message }</Text>
+    </Box> 
+  );
+};
+
+ChatMessageCard.propTypes = {
+  msg: PropTypes.shape({
+    message: PropTypes.string,
+    nickname: PropTypes.string,
+    time: PropTypes.string
+  }),
+};
+
+export default ChatMessageCard;
