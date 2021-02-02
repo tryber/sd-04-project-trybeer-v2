17443f9a6102dfce62abe29cf5a7d77b42443e73
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2021-02-02 18:59:31 -0300

    Revert "Req 8"
    This reverts commit 5b1471a96f8748ea253b4c2fd7e3cee4cc6a9811.

--- front-end/src/pages/admin/chatAdmin.jsx
+++ front-end/src/pages/admin/chatAdmin.jsx
@@ -4,12 +4,6 @@ import TopBar from '../../components/ClientBar';
 
 const socket = io('http://localhost:3001');
 
-// AGORA DEVO CRIAR as conversas do adm com o usuário, poderei salvar as conversas
-// no mesmo local de conversas do usuário, porém o nickname será diferente, então
-// talvez não seja possível
-// TALVEZ deva salvar toda a conversa exclusivamente no banco do administrator
-//  ou a conversa inteira no banco do administrator
-
 const ChatAdmin = () => {
   const loginInStorage = JSON.parse(localStorage.getItem('user'));
 
@@ -17,25 +11,16 @@ const ChatAdmin = () => {
     const divBoxMessages = document.getElementById('messageBoxes');
     const cardMessages = document.createElement('div');
     cardMessages.setAttribute('className', 'card');
-    cardMessages.setAttribute('data-testid', 'containerChat');
     const nameBox = document.createElement('p');
-    nameBox.setAttribute('className', 'card-title');
-    nameBox.setAttribute('data-testid', 'profile-name');
-    const hourBox = document.createElement('p');
+    nameBox.setAttribute('className', 'card-title' )
+    const hourBox = document.createElement('p')
     hourBox.innerHTML = data.date;
     nameBox.innerHTML = data.nickname;
-    hourBox.setAttribute('data-testid', 'last-message');
     cardMessages.appendChild(nameBox);
-    cardMessages.appendChild(hourBox);
+    cardMessages.appendChild(hourBox)
     divBoxMessages.appendChild(cardMessages);
-  };
 
-  const emptyMessages = () => {
-    const messageAlert = document.createElement('h1');
-    messageAlert.innerHTML = 'Nenhuma conversa por aqui';
-    messageAlert.setAttribute('data-testid', 'text-for-no-conversation');
-    const divBoxMessages = document.getElementById('messageBoxes');
-    divBoxMessages.appendChild(messageAlert);
+
   };
 
   if (loginInStorage.role === 'administrator') {
@@ -44,18 +29,15 @@ const ChatAdmin = () => {
     socket.on('listByName', (data) => {
       const lastMessages = [];
 
-      data.forEach((item) => {
-        lastMessages.push(item[item.length - 1]);
+      data.forEach((item, index) => {
+        lastMessages.push(item[item.length -1])
       });
 
-      lastMessages.forEach((item) => {
-        if (item !== undefined) {
-          createCard(item);
-        } else {
-          return emptyMessages();
+      lastMessages.forEach((item)=> {
+        if(item !== undefined){
+          createCard(item)
         }
-        return null;
-      });
+      })
     });
 
     return (
@@ -67,9 +49,9 @@ const ChatAdmin = () => {
           isDetails
         />
         <div className="container">
-          <h1 className="messages" id="messageBoxes">
-            Todas as conversas
-          </h1>
+            <h1 className="messages" id="messageBoxes">
+              Todas as conversas
+            </h1>
         </div>
       </div>
     );
