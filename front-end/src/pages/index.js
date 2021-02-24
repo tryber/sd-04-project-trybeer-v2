e9bed9890affd4c416858f1713f79ae925e55177
pedrotpo <pedrotpoliveira@gmail.com>
2021-02-24 15:13:59 -0300

    Final Commit

--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -1,6 +1,5 @@
 export { default as Chat } from './Chat';
 export { default as Checkout } from './Checkout';
-export { default as Home } from './Home';
 export { default as Login } from './Login';
 export { default as NotFound } from './NotFound';
 export { default as Orders } from './Orders';
